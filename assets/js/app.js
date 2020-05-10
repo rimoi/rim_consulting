5fb819c61d688532db1a536a09ab752eae94109f
sidikhalifa <sidi.khalifa@live.fr>
2020-05-10 12:51:34 +0200

    add rim consulting

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+require('bootstrap');
+import './ext/jquery.stellar.min';
+import './ext/jquery.magnific-popup.min';
+import './ext/smoothscroll';
+import './ext/custom';
+import './my/script';
+
+import '../css/app.css';
+
+// PARALLAX EFFECT
+$(document).ready(function() {
+    // $.stellar({
+    //     horizontalScrolling: false,
+    // });
+});
+
+
